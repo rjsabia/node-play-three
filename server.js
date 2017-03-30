debe47e6d8267017628172f81bbc97b342a1e92a
Russell Sabia <rjsabia@gmail.com>
2017-03-29 21:05:07 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+//variables
 var express = require('express');
 var app = express();
-
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+//middleware
+app.use(morgan('combined'));
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+//routes
 app.get('/', function(req, res){
 	res.send('Hello World');
 })
+//turn on server
 app.listen(3000);
